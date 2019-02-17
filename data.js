86dc1ca05dc17b3f686006771e398d9505ec5cdb
Vusic360 <47711749+Vusic360@users.noreply.github.com>
2019-02-17 15:33:04 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,183 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-haveli---jalandhar",
+      "name": "HAVELI - JALANDHAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -1.256856688724925,
+        "pitch": -0.020840476538783292,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4530996045248656,
+          "pitch": -0.5882145303595845,
+          "rotation": 0,
+          "target": "1-haveli---jalandhar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-haveli---jalandhar",
+      "name": "HAVELI - JALANDHAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.23121117218508047,
+        "pitch": -0.05161613378504626,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8683117196421026,
+          "pitch": -0.5443028380786892,
+          "rotation": 0,
+          "target": "0-haveli---jalandhar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8273408446887949,
+          "pitch": -0.2774640357073199,
+          "title": "HAVELI - JALANDHAR",
+          "text": "PARKING&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-gurudwara-darbar-sahib---tarn-taran",
+      "name": "Gurudwara Darbar Sahib - Tarn Taran",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -3.133133705479956,
+        "pitch": -0.019310139694177053,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.3090737051794274,
+          "pitch": -0.42355028890264457,
+          "title": "Gurudwara Darbar Sahib&nbsp;",
+          "text": "TARN TARAN"
+        }
+      ]
+    },
+    {
+      "id": "3-golden-temple",
+      "name": "Golden Temple",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.13149138013746686,
+        "pitch": -0.03588134999379733,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15482146267961738,
+          "pitch": -0.6236751443796109,
+          "rotation": 6.283185307179586,
+          "target": "1-haveli---jalandhar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7649412572619614,
+          "pitch": -0.2720994728791677,
+          "title": "Bunga Ramgharia",
+          "text": "<p class=\"MsoNormal\" style=\"margin-bottom:0cm;margin-bottom:.0001pt;text-align:\njustify;text-justify:inter-ideograph;line-height:normal\"><span style=\"font-size:9.0pt;mso-bidi-font-size:11.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;\">Ramgarhia\nBunga (Bunga means \"mansion\"), is a three-storeyed marvel of Sikh\narchitecture located in the vicinity of Shri Darbar Sahib Amritsar. Ramgarhia\nbunga, a pre-Ranjit Singh structure built by Sikh warrior and Ramgarhia Misl Chief\nJassa Singh Ramgarhia in 1755, is the only surviving example of Bunga\narchitecture typology. It was constructed to fortifying the area to protect the\nholy complex from outside invasion.<o:p></o:p></span></p>"
+        },
+        {
+          "yaw": -0.9737465710294053,
+          "pitch": -0.2796066894445328,
+          "title": "Akal Takht",
+          "text": "<p class=\"MsoNormal\" style=\"margin-bottom:0cm;margin-bottom:.0001pt;text-align:\njustify;text-justify:inter-ideograph;line-height:normal\"><span style=\"font-size:9.0pt;mso-bidi-font-size:11.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;\">Akal\nTakht (Punjabi: </span><span style=\"font-size:9.0pt;mso-bidi-font-size:11.0pt;\nfont-family:&quot;Nirmala UI&quot;,&quot;sans-serif&quot;\">ਅਕਾਲ</span><span style=\"font-size:9.0pt;\nmso-bidi-font-size:11.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;\"> </span><span style=\"font-size:9.0pt;mso-bidi-font-size:11.0pt;font-family:&quot;Nirmala UI&quot;,&quot;sans-serif&quot;\">ਤਖ਼ਤ</span><span style=\"font-size:9.0pt;mso-bidi-font-size:11.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;\">,\nAkāl Taḵẖt) means the Throne of the Immortal and is the highest political\ninstitution of the Sikhs. \"Akal\" means \"The Timeless One\" -\nanother term for God. \"Takht\" means \"throne\" in Persian.\nThe Akal Takhat is an impressive building that sits directly in front of the\ncauseway leading to the Golden Temple in Amritsar. The Akal Takht was founded\nby Guru Hargobind on June 15, 1606 and was established as the place from which\nthe spiritual and temporal concerns of the Sikh community could be acted upon.<o:p></o:p></span></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
